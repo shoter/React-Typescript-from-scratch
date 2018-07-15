85f2d893c9381e9db0b65058a3e4735b5654a519
shoter <lubiebardzozelki@gmail.com>
2018-07-15 03:12:46 +0200

    Removed dist directory. Replaced it with build directory.

--- dist/typescript-compiled/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Person {
-    firstName: string;
-    secondName: string;
-    constructor(firstName: string, secondName: string);
-    hello(): string;
-}
-declare let person: Person;
-declare let greeting: string;
-//# sourceMappingURL=index.d.ts.map
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -23,7 +23,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, "/dist/")
+        path: path.join(__dirname, "/build/")
     },
     devServer: {
         before: function (app) {
